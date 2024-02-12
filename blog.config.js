3a4fc8d047b7c7f8dc185c64355ff83cb8169d43
KingCode <53894312+KingCode-01@users.noreply.github.com>
2024-02-12 18:10:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -203,7 +203,7 @@ const BLOG = {
   DIFY_CHATBOT_BASE_URL: process.env.NEXT_PUBLIC_DIFY_CHATBOT_BASE_URL || '',
   DIFY_CHATBOT_TOKEN: process.env.NEXT_PUBLIC_DIFY_CHATBOT_TOKEN || '',
   // 悬浮挂件
-  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
+  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || false, // 是否显示宠物挂件
   WIDGET_PET_LINK:
         process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
         'https://imuncle.github.io/live2d/model/platelet-3/kesyoban.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
